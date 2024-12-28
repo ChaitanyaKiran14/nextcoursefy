dcdf17c0f98046c2051755e9aaf4bbe991411370
ChaitanyaKiran14 <g.chaitanya1419@gmail.com>
2024-12-28 20:39:11 +0530

    router navigation handling

--- src/app/Components/navigation.tsx
+++ src/app/Components/navigation.tsx
@@ -1,16 +1,18 @@
+"use client"
 import Link from "next/link"
-
+import { usePathname } from "next/navigation"
 
 const Navigation  = () => {
+    const pathname = usePathname()
     return (
         <div>
-             <div className="flex items-center justify-center mt-10"> 
-            <Link  href={'/'} className="mr-5 text-yellow-400">Home</Link>
-            <Link  href={'/about'} className="mr-5 text-yellow-400">About</Link>
-            <Link  href={'/products/1'} className="mr-5 text-yellow-400">Products</Link>
+             <div className="flex items-center justify-center mt-10  bg-transparent"> 
+            <Link  href={'/'} className={pathname ==="/"?  "font-bold mr-5 text-blue-700" : "mr-5"}      >Home</Link>
+            <Link  href={'/about'} className={pathname ==="/about"?  "font-bold mr-5 text-blue-700" : "mr-5"} >About</Link>
+            <Link  href={'/products/1'} className={pathname ==="/products/1"?  "font-bold mr-5 text-blue-700" : "mr-5"} >Products</Link>
 
             <Link href={"/login"} className="text-green-400 ml-52 mr-10"> Login</Link>
-            <Link href={"/register"} className="text-blue-600"> Register</Link>
+            <Link href={"/register"} className="text-yellow-900"> Register</Link>
 
         </div>
        
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,16 @@
-const Blog = () => {
+"use client"
 
-    console.log("ikdsf")
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+const Blog = () => {
+    //invoke the useRouter hook 
+    const router = useRouter()
+
     return(
         <div className="items-center justify-items-center m-10  ">
-            <h1>Hey Chaitu this is from Blog</h1>
+            <h1>Hey Chaitu this is from About</h1>
+            <br/>
+            <button onClick={()=> router.push("/")}  className="bg-blue-500 p-2 rounded-md">Go Home</button>
            
         </div>
        
